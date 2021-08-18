688295c8434347022eaba892e2d777dfa23abab2
ryanfirmanda <62127993+ryanfirmanda@users.noreply.github.com>
2021-08-18 12:48:45 +0700

    Add files via upload

---
+++ js/testi.js
@@ -0,0 +1,8 @@
+$('.testimonials-slider').bxSlider({
+       slideWidth: 800,
+       minSlides: 1,
+       maxSlides: 1,
+       slideMargin: 32,
+       auto: true,
+       autoControls: true
+     });
